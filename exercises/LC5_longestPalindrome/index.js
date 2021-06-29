d650c36c17f8677e4c7d5994a093e6b7b1e9917d
Howie Z <masterxdh20@gmail.com>
2021-06-29 00:22:23 -0400

    solved #5 longest palindrome

--- exercises/LC5_longestPalindrome/index.js
+++ exercises/LC5_longestPalindrome/index.js
@@ -4,6 +4,26 @@
 // longestPalindrome("abba") --> "abba"
 // longestPalindrome("a") --> "a"
 
-function longestPalindrome(s) {}
+function longestPalindrome(s) {
+  let starting = 0;
+  let maxLength = 1;
+
+  function expandMid(left,right){
+    while (left >= 0 && right < s.length && s[left] === s[right]){
+      const currentLength = right - left +1;
+      if (currentLength > maxLength){
+        maxLength=currentLength;
+        starting = left;
+      }
+      left=left-1
+      right = right+1 
+    }
+  }
+  for (let i=0; i<s.length; i++){
+    expandMid(i-1,i+1);
+    expandMid(i,i+1);
+  }
+  return s.slice(starting,starting+maxLength);
+}
 
 module.exports = longestPalindrome;
